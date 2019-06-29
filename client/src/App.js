a5635cbc307057165e7b78ea1134219e6db0dca2
nicorponic <n1co_frago@hotmail.com>
2019-06-30 01:36:44 +0300

    Add NavBar. Add Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import UsersContainer from './components/UsersContainer';
 import { Route } from "react-router-dom";
-
+import NavBar from './NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <main>
+        <NavBar/>
           <Route exact path="/" component={UsersContainer} />
         </main>
       </div>
---
+++ client/src/components/Register.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import request from "superagent";
+import { Link } from "react-router-dom";
+
+export default class Register extends Component {
+
+}
